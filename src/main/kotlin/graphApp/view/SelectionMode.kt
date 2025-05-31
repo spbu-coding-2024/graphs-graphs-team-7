40baeec946f90c26709c2911452b27c930b7671e
Vladimir Khmelev <khmelevvladimir2006@gmail.com>
2025-05-31 19:51:10 +0300

    implement selection modes for Dijkstra algorithm integration

--- src/main/kotlin/graphApp/view/SelectionMode.kt
+++ src/main/kotlin/graphApp/view/SelectionMode.kt
@@ -1,2 +1,9 @@
 package graphApp.view
 
+// Для алгоритма Дейкстры
+
+sealed class SelectionMode {
+    object NONE : SelectionMode()
+    object DIJKSTRA_START : SelectionMode()
+    object DIJKSTRA_END : SelectionMode()
+}
