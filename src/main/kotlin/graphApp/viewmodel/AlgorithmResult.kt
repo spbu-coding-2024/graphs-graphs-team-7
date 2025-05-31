db44d01a67ea2764f2b08a232cb2f2f9eab70d19
Vladimir Khmelev <khmelevvladimir2006@gmail.com>
2025-05-31 20:01:19 +0300

    add AlgorithmResult model for graph algorithm output . Implemented sealed class with 3 result types

--- src/main/kotlin/graphApp/viewmodel/AlgorithmResult.kt
+++ src/main/kotlin/graphApp/viewmodel/AlgorithmResult.kt
@@ -1,2 +1,10 @@
 package graphApp.viewmodel
 
+import graphApp.model.graph.Vertex
+
+sealed class AlgorithmResult {
+    data class ShortestPath(val path: List<Vertex>, val distance: Double) : AlgorithmResult()
+    data class ConnectedComponents(val components: List<List<Vertex>>) : AlgorithmResult()
+    data class MessageResult(val message: String) : AlgorithmResult()
+}
+
