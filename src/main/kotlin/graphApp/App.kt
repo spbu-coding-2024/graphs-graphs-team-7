c63c08147c231a81bdd1e76779d54a92089c3dfa
Vladimir Khmelev <khmelevvladimir2006@gmail.com>
2025-05-31 18:54:58 +0300

    changed App.kt

--- src/main/kotlin/graphApp/App.kt
+++ src/main/kotlin/graphApp/App.kt
@@ -1,35 +1,36 @@
 package graphApp
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import graphApp.view.MainView
 import graphApp.viewmodel.GraphViewModel
+import java.awt.Dimension
 
 fun main() = application {
-    var windows by remember { mutableStateOf(1) }
+    var windows by remember { mutableStateOf(listOf(0 to GraphViewModel())) } // Инициализация первого окна сразу
 
-    repeat(windows) { index ->
+    windows.forEach { (id, viewModel) ->
         Window(
-            title = "Graph Visualizer ${index + 1}",
-            onCloseRequest = { if (windows > 1) windows-- else exitApplication() }
+            title = "Graph Visualizer ${id + 1}",
+            onCloseRequest = {
+                windows = windows.filterNot { it.first == id }
+                if (windows.isEmpty()) exitApplication()
+            }
         ) {
-            val viewModel = remember { GraphViewModel() }
-            MainView(viewModel)
-        }
-    }
 
-    Window(
-        title = "Controller",
-        onCloseRequest = ::exitApplication
-    ) {
-        Button(onClick = { windows++ }) {
-            Text("Open New Window")
+            LaunchedEffect(Unit){
+                window.minimumSize = Dimension(1024, 768)
+            }
+
+            MainView(
+                viewModel = viewModel,
+                onNewWindow = { windows = windows + (windows.size to GraphViewModel()) },
+                onCloseWindow = {
+                    windows = windows.filterNot { it.first == id }
+                    if (windows.isEmpty()) exitApplication()
+                }
+            )
         }
     }
 }
